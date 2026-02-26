ba36e9a9a96cf7d4365ff160352f33ec2e5bb992
Jyotirmoy Bhowmik <jyotu@live.com>
2026-02-26 15:19:35 +0545

    fix backend

--- apps/backend/src/suggestions/suggestions.module.ts
+++ apps/backend/src/suggestions/suggestions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SuggestionsService } from './suggestions.service';
 import { SuggestionsController } from './suggestions.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, JwtModule.register({ secret: process.env.JWT_SECRET || 'secret' })],
     controllers: [SuggestionsController],
     providers: [SuggestionsService],
 })
